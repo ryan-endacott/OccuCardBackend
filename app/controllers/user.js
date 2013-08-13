48c39543bba2465aef10d3e675c6b3d980892988
Ryan Endacott <rzeg24@gmail.com>
2013-08-12 21:52:24 -0500

    start of user controller

---
+++ app/controllers/user.js
@@ -0,0 +1,17 @@
+var db = require('../db'),
+  user = db.User,
+  errors = require('../errors'),
+  badRequest = errors.badRequest;
+
+exports.register = function(req, res){
+  var user = new User(req.body.user);
+  user.generateToken();
+  user.save(function(err, user) {
+    if (err) badRequest(err);
+    res.json(user);
+  });
+};
+
+exports.update = function(req, res){
+
+};
