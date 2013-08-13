f224450d9c10ed1f328b403e9a301425f7fb7094
Ryan Endacott <rzeg24@gmail.com>
2013-08-12 21:52:16 -0500

    add user routes

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,12 @@
 // Set up routes for the app
 
+// Controllers
+var index = require('../app/controllers/index'),
+  user = require('../app/controllers/user');
+
 module.exports = function(app) {
 
-	var index = require('../app/controllers/index');
 	app.get('/', index.index);
+  app.post('/register', user.register);
+  app.post('/user/update', user.update);
 }
