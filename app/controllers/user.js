e40bda341414cfb7aef0f0133f66bd7c338787ac
Ryan Endacott <rzeg24@gmail.com>
2013-08-12 22:47:12 -0500

    got user controller error handling working

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,13 +1,13 @@
 var db = require('../db'),
   User = db.User,
   errors = require('../errors'),
-  badRequest = errors.badRequest;
+  badRequest = errors.BadRequestError;
 
-exports.register = function(req, res){
+exports.register = function(req, res, next){
   var user = new User(req.body.user);
   user.generateToken();
   user.save(function(err, user) {
-    if (err) badRequest(err);
+    if (err) return badRequest(err, res);
     res.json(user);
   });
 };
