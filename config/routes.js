031b73360b0f12cc68aa1dd0947e808bff8f895f
Ryan Endacott <rzeg24@gmail.com>
2013-08-14 17:00:30 -0500

    add route for add contact by email

--- config/routes.js
+++ config/routes.js
@@ -12,4 +12,5 @@ module.exports = function(app) {
   app.post('/user/register', user.register);
   app.post('/user/update', auth, user.update);
   app.get('/contacts/all', auth, contacts.all);
+  app.post('/contacts/add/email', auth, contacts.addByEmail);
 }
