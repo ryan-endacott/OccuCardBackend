7e5b5aaba24943b8d9afc2a0043da1de7a729db1
Ryan Endacott <rzeg24@gmail.com>
2013-08-15 18:38:12 -0500

    start adding password and username stuff to db

--- app/db.js
+++ app/db.js
@@ -13,6 +13,15 @@ db.once('open', function() {
 });
 
 var userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
   apiToken: String,
   firstName: String,
   lastName: String,
@@ -22,9 +31,60 @@ var userSchema = new Schema({
   contacts: [{type : mongoose.Schema.ObjectId, ref : 'User'}]
 });
 
-userSchema.methods.generateApiToken = function() {
-  this.apiToken = uuid.v4();
+userSchema.pre('save', function(next) {
+  // Maybe make this pre init
+  // If no api token already added
+  if (!user.apiToken || user.apiToken.length != 36) {
+    this.apiToken = uuid.v4();
+    next();
+  }
+});
+
+// Bcrypt middleware for hashing password
+userSchema.pre('save', function(next) {
+  var user = this;
+
+  if(!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if(err) return next(err);
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if(err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Password verification
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if(err) return cb(err);
+    cb(null, isMatch);
+  });
 };
+
+// Use the LocalStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a username and password), and invoke a callback
+//   with a user object.  In the real world, this would query a database;
+//   however, in this example we are using a baked-in set of users.
+passport.use(new LocalStrategy(function(username, password, done) {
+  User.findOne({ username: username }, function(err, user) {
+    if (err) { return done(err); }
+    if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) return done(err);
+      if(isMatch) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Invalid password' });
+      }
+    });
+  });
+}));
+
 
 // Don't return API Token of any user
 // specify the transform schema option
