8e09579b99c717d7f4bc2cb677ec67ef6d912c4c
Ryan Endacott <rzeg24@gmail.com>
2013-08-15 19:32:00 -0500

    switch to passport-http

--- config/auth.js
+++ config/auth.js
@@ -19,34 +19,16 @@ module.exports = {
   },
 
   setupPassport: function setupPassport() {
-    // Use the LocalStrategy within Passport.
-    //   Strategies in passport require a `verify` function, which accept
-    //   credentials (in this case, a username and password), and invoke a callback
-    //   with a user object.  In the real world, this would query a database;
-    //   however, in this example we are using a baked-in set of users.
-    passport.use(new LocalStrategy(function(username, password, done) {
-      User.findOne({
-        username: username
-      }, function(err, user) {
-        if (err) {
-          return done(err);
-        }
-        if (!user) {
-          return done(null, false, {
-            message: 'Unknown user ' + username
-          });
-        }
-        user.comparePassword(password, function(err, isMatch) {
-          if (err) return done(err);
-          if (isMatch) {
-            return done(null, user);
-          } else {
-            return done(null, false, {
-              message: 'Invalid password'
-            });
-          }
+    passport.use(new BasicStrategy(
+      function(email, password, done) {
+        User.findOne({ email: email }, function (err, user) {
+          if (err) { return done(err); }
+          if (!user) { return done(null, false); }
+          if (!user.verifyPassword(password)) { return done(null, false); }
+          return done(null, user);
         });
-      });
-    }));
+      }
+    ));
+  }
 
-  };
+};
