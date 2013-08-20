a5df220a68f44ac5345147fccdc95d1304e8067d
Ryan Endacott <rzeg24@gmail.com>
2013-08-19 21:23:04 -0500

    remove express session and cookies, not needed

--- config/bootup.js
+++ config/bootup.js
@@ -18,8 +18,6 @@ module.exports = function(app) {
 		app.use(express.logger('dev'));
 		app.use(express.bodyParser());
 		app.use(express.methodOverride());
-		app.use(express.cookieParser('your secret here'));
-		app.use(express.session());
 		app.use(passport.initialize());
 		app.use(app.router);
 		app.use(express.static(__dirname + '/../public'));
