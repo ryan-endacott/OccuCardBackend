60618b972f9c74529d45213ffb064aedf5d672ed
Ryan Endacott <rzeg24@gmail.com>
2013-08-24 00:04:02 -0500

    Cannot add self as a contact and cannot add contacts more than once

--- app/controllers/contacts.js
+++ app/controllers/contacts.js
@@ -14,10 +14,23 @@ exports.all = function(req, res) {
 exports.addByEmail = function(req, res) {
   User.findOne({email: req.body.email}, function(err, newContact) {
     if (err) return internalError(err, res);
+
     if (!newContact) return badRequest({
       message: 'No contact found with that email.'
     }, res);
-    console.log('NUMBAS:  ' + req.user.contacts.indexOf(newContact._id));
+
+    if (req.user._id === newContact._id) {
+      return badRequest({
+        message: 'You cannot add yourself as a contact.'
+      }, res);
+    }
+
+    if (req.user.contacts.indexOf(newContact._id) != -1) {
+      return badRequest({
+        message: 'You have already added that contact.'
+      }, res);
+    }
+
     req.user.contacts.push(newContact);
     req.user.save(function(err, user) {
       if (err) return internalError(err, res);
